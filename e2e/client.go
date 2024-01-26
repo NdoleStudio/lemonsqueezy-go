711ad2de2250a5d2b90baec69183a1d4c19fd17d
Acho Arnold <arnold@ndolestudio.com>
2024-01-26 08:08:47 +0200

    Fix the .env file

--- e2e/client.go
+++ e2e/client.go
@@ -3,8 +3,8 @@ package e2e
 import (
 	"os"
 
-	lemonsqueezy "github.com/NdoleStudio/lemonsqueezy-go"
-	_ "github.com/joho/godotenv/autoload" // import LEMONSQUEEZY_API_KEY from .env file
+	"github.com/NdoleStudio/lemonsqueezy-go"
+	_ "github.com/joho/godotenv/autoload" // import LEMON_SQUEEZY_API_KEY from .env file
 )
 
-var client = lemonsqueezy.New(lemonsqueezy.WithAPIKey(os.Getenv("LEMONSQUEEZY_API_KEY")))
+var client = lemonsqueezy.New(lemonsqueezy.WithAPIKey(os.Getenv("LEMON_SQUEEZY_API_KEY")))
