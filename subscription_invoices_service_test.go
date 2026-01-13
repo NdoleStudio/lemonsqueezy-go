a3d4ad05fa5431af8177e57dda30d90d0e6bac1f
Acho Arnold <arnold@ndolestudio.com>
2026-01-13 23:14:02 +0200

    Fixed the tests

--- subscription_invoices_service_test.go
+++ subscription_invoices_service_test.go
@@ -61,7 +61,7 @@ func TestSubscriptionInvoicesService_List(t *testing.T) {
 	client := New(WithBaseURL(server.URL))
 
 	// Act
-	invoices, response, err := client.SubscriptionInvoices.List(context.Background())
+	invoices, response, err := client.SubscriptionInvoices.List(context.Background(), nil)
 
 	// Assert
 	assert.Nil(t, err)
@@ -83,7 +83,7 @@ func TestSubscriptionInvoicesService_ListWithError(t *testing.T) {
 	client := New(WithBaseURL(server.URL))
 
 	// Act
-	_, response, err := client.SubscriptionInvoices.List(context.Background())
+	_, response, err := client.SubscriptionInvoices.List(context.Background(), nil)
 
 	// Assert
 	assert.NotNil(t, err)
@@ -99,18 +99,30 @@ func TestSubscriptionInvoicesService_Generate(t *testing.T) {
 	t.Parallel()
 
 	// Arrange
-	server := helpers.MakeTestServer(http.StatusOK, stubs.SubscriptionInvoicesListResponse())
+	server := helpers.MakeTestServer(http.StatusOK, stubs.SubscriptionInvoicesGenerateResponse())
 	client := New(WithBaseURL(server.URL))
 
 	// Act
-	invoices, response, err := client.SubscriptionInvoices.List(context.Background())
+	invoice, response, err := client.SubscriptionInvoices.Generate(context.Background(), "1234", map[string]string{
+		"address":  "123 Main St",
+		"city":     "Anytown",
+		"country":  "US",
+		"name":     "John Doe",
+		"notes":    "Thank you for your business",
+		"state":    "CA",
+		"zip_code": "12345",
+	})
 
 	// Assert
 	assert.Nil(t, err)
 
 	assert.Equal(t, http.StatusOK, response.HTTPResponse.StatusCode)
-	assert.Equal(t, stubs.SubscriptionInvoicesListResponse(), *response.Body)
-	assert.Equal(t, 1, len(invoices.Data))
+	assert.Equal(t, stubs.SubscriptionInvoicesGenerateResponse(), *response.Body)
+	assert.Equal(
+		t,
+		"https://app.lemonsqueezy.com/my-orders/c1e4de31-b4cf-4668-a6fe-019d071d41ab/invoice/download?address=123%20Main%20St&city=Anytown&country=US&name=John%20Doe&notes=Thank%20you%20for%20your%20business&state=CA&zip_code=12345&signature=c21a17a13d9deeacc99ff52144a06b49df141af37dd668cc70a76bcc8022888e",
+		invoice.Meta.Urls.DownloadInvoice,
+	)
 
 	// Teardown
 	server.Close()
